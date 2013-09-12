42f1c93e3a14bd4a7b315263ded8b4445e78fc0c
Saher El-Neklawy <saher.neklawy@gmail.com>
2013-09-12 11:50:04 +0200

    Adding delayed deletes to mongoid

--- config/initializers/mongoid_extensions.rb
+++ config/initializers/mongoid_extensions.rb
@@ -3,3 +3,22 @@ if defined?(::Mongoid::Document)
   Mongoid::Document.send(:include, RailsAdmin::Adapters::Mongoid::Extension)
 end
 I18n.backend = I18n::Backend::SpecBackend.new
+
+
+module Mongoid
+    module Document
+        class DelayedDeleteJob < Struct.new(:klass, :id)
+            def perform
+                old_flag = Delayed::Worker.delay_jobs
+                Delayed::Worker.delay_jobs = false
+                klass.find(id).destroy
+                Delayed::Worker.delay_jobs = old_flag
+            end
+        end
+
+        def destroy_delayed
+            Delayed::Job.enqueue(DelayedDeleteJob.new(self.class, self.id), queue: 'delayed_destroy')
+            true
+        end
+    end
+end
